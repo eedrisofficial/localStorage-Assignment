9f5967f7807f15b74cdf8d8e4d235718d2cd2e39
eedrisofficial <eedrisofficial.gmail.com>
2023-09-12 22:05:44 +0100

    update

--- signin.js
+++ signin.js
@@ -4,8 +4,8 @@ formData.addEventListener("click", userData);
 function userData(e) {
   e.preventDefault();
 
-  let usersEmail = document.getElementById("email").value;
-  let Password = document.getElementById("password").value;
+  let usersEmail = document.getElementById("lEmail").value;
+  let Password = document.getElementById("lPassword").value;
 
   // Collecting the details as object
   const userDetails = {
@@ -16,4 +16,5 @@ function userData(e) {
   // Saving the details collected to local storage
   localStorage.setItem("database", JSON.stringify(userDetails));
   let dataRetrieve = JSON.parse(localStorage.getItem("database"));
+  console.log(dataRetrieve);
 }
