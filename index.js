00f80a4aa7d768a71828340669669daeeee97918
eedrisofficial <eedrisofficial.gmail.com>
2023-09-13 03:49:45 +0100

    errors debugged

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 //Targeting the form by its id
-let formData = document.getElementById("formContainer");
-formData.addEventListener("click", userData);
-function userData(e) {
-  e.preventDefault();
+const formData = document.getElementById("formContainer");
+formData.addEventListener("submit", function (event) {
+  event.preventDefault();
   let usersName = document.getElementById("Name").value;
   let usersEmail = document.getElementById("email").value;
   let Password = document.getElementById("password").value;
   let confirmPassword = document.getElementById("confirmPassword").value;
+
+  //validation
+  if (
+    usersName === "" ||
+    usersEmail === "" ||
+    Password === "" ||
+    confirmPassword === ""
+  ) {
+    // Display an error message if any field is empty
+    alert("Please fill in all fields.");
+  } else if (Password.length !== 6) {
+    alert("password must be more than 5 character");
+  } else if (Password !== confirmPassword) {
+    // Display an error message if passwords do not match
+    alert("Passwords do not match.");
+  } else {
+    window.location.href = "/signin.html";
+  }
 
   // Collecting the details as object
   const userDetails = {
@@ -19,5 +36,4 @@ function userData(e) {
   // Saving the details collected to local storage
   localStorage.setItem("database", JSON.stringify(userDetails));
   let dataRetrieve = JSON.parse(localStorage.getItem("database"));
-  window.location.href = "/signin.html";
-}
+});
--- signin.js
+++ signin.js
@@ -1,11 +1,20 @@
 //Targeting the form by its id
 let formData = document.getElementById("formContainer");
-formData.addEventListener("click", userData);
-function userData(e) {
-  e.preventDefault();
+formData.addEventListener("submit", function (event) {
+  event.preventDefault();
 
+  // collecting input value
   let usersEmail = document.getElementById("lEmail").value;
   let Password = document.getElementById("lPassword").value;
+
+  // validation
+  if (usersEmail === "" || Password === "") {
+    alert("please fill the input to continue");
+  } else if (Password.length !== 6) {
+    alert("password must be more than 5 character");
+  } else {
+    window.location.href = "/index.html";
+  }
 
   // Collecting the details as object
   const userDetails = {
@@ -16,5 +25,4 @@ function userData(e) {
   // Saving the details collected to local storage
   localStorage.setItem("database", JSON.stringify(userDetails));
   let dataRetrieve = JSON.parse(localStorage.getItem("database"));
-  window.location.href = "/index.html";
-}
+});
